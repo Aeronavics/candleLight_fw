325aa22795a77183c400f4368289609f2acccd9b
Anton Slooten <anton.slooten@gmail.com>
2019-12-06 14:50:43 +1300

    Updating config to reflect hardware differences
    This is just the LED's.

--- include/config.h
+++ include/config.h
@@ -48,13 +48,13 @@ THE SOFTWARE.
 	#define CAN_S_GPIO_Port GPIOC
 
 	#define LED1_Pin GPIO_PIN_0
-	#define LED1_Mode GPIO_MODE_OUTPUT_OD
-	#define LED1_GPIO_Port GPIOA
+	#define LED1_Mode GPIO_MODE_OUTPUT_PP
+	#define LED1_GPIO_Port GPIOB
 	#define LED1_Active_High 0
 
-	#define LED2_GPIO_Port GPIOA
+	#define LED2_GPIO_Port GPIOB
 	#define LED2_Pin GPIO_PIN_1
-	#define LED2_Mode GPIO_MODE_OUTPUT_OD
+	#define LED2_Mode GPIO_MODE_OUTPUT_PP
 	#define LED2_Active_High 0
 
 #elif BOARD == BOARD_cantact
